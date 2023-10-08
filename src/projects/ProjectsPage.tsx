f7ad716f5ed727e3e08ae990d4debbceaa801c27
NAKAO RYOTA <premium.developer1122.gmail.com>
2023-10-08 21:41:37 +0900

    HTTP GET

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,40 @@
-import { useState } from "react";
-import { MOCK_PROJECTS } from "./MockProjects";
+import { useEffect, useState } from "react";
 import { Project } from "./Project";
 import { ProjectList } from "./ProjectList";
+import { projectAPI } from "./projectAPI";
 
 export const ProjectPage = () => {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleMoreClick = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
+
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.get(currentPage);
+        setError("");
+        if (currentPage === 1) {
+          setProjects(data);
+        } else {
+          setProjects((project) => [...project, ...data]);
+        }
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, [currentPage]);
+
   const saveProject = (project: Project) => {
     let updataedProjects = projects.map((p: Project) => {
       return p.id === project.id ? project : p;
@@ -14,7 +44,36 @@ export const ProjectPage = () => {
   return (
     <>
       <h1>Projects</h1>
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse"></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
       <ProjectList onSave={saveProject} projects={projects} />
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
     </>
   );
 };
